df349a1ab2102bad47da33212d969baf23b70a03
Lynn-Myat-Bhone <lynnmyatbhone311@gmail.com>
2025-01-11 22:27:23 +0630

    bug fix

--- Micrograd from skatch/model_test.py
+++ Micrograd from skatch/model_test.py
@@ -17,8 +17,8 @@ ygt = [1.0,-1.0,-1.0,1.0]  # Targets
 
 
 y_pred = [n(x) for x in xs]
-learning_rate=.001
-epochs = 70
+learning_rate = 0.01
+epochs = 100
 for epoch in range(epochs):
     # Forward pass
     y_pred = [n(x) for x in xs]
@@ -26,7 +26,10 @@ for epoch in range(epochs):
     # Compute loss
     loss = sum((correct - error)**2 for error, correct in zip(y_pred, ygt))
     print(f"Epoch {epoch}, Loss: {loss.data}")
-    # print(ygt)
+    
+    for p in n.parameters():
+        p.grad = 0.0
+        
     for i, (x, pred) in enumerate(zip(xs, y_pred)):
         print(f"Input {i}: {x}, Prediction: {pred.data}")
     # Backward pass
